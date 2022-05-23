c750ea5e3037f1b910c306678e23496c5ccf91d0
Vaishnavi <vaishnavi.chaurasia_cs18@gla.ac.in>
2022-05-23 19:09:11 +0530

    Time: 1 ms (86.49%), Space: 54.5 MB (54.96%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int start = 0;
+        int end = s.length - 1;
+        
+        
+        while(start<end){
+            
+            swap(s,start,end);
+            start++;
+            end--;
+            
+        }
+        
+    }
+    
+    public static void swap(char[] s , int start , int end){
+        
+        char temp = s[start];
+        s[start] = s[end];
+        s[end]= temp;
+        
+    }
+}
